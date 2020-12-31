9d44eb7d92269c2f6561464535a461b96f492aaa
iqbalfsr-coder <muh18557@gmail.com>
2021-01-01 00:12:40 +0700

    Ngayom

--- application/views/home/regis.php
+++ application/views/home/regis.php
@@ -3,72 +3,122 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Registration</title>
-    <link href="<?= base_url('assets/eshop/'); ?>css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/eshop/'); ?>css/font-awesome.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/eshop/'); ?>css/prettyPhoto.css" rel="stylesheet">
-    <link href="<?= base_url('assets/eshop/'); ?>css/price-range.css" rel="stylesheet">
-    <link href="<?= base_url('assets/eshop/'); ?>css/animate.css" rel="stylesheet">
-    <link href="<?= base_url('assets/eshop/'); ?>css/main.css" rel="stylesheet">
-    <link href="<?= base_url('asset/eshop/'); ?>css/responsive.css" rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-    <link rel="shortcut icon" href="<?= base_url('assets/eshop/'); ?>images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= base_url('assets/eshop/'); ?>images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= base_url('assets/eshop/'); ?>images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= base_url('assets/eshop/'); ?>images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?= base_url('assets/eshop/'); ?>images/ico/apple-touch-icon-57-precomposed.png">
+    <title>Login</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <!-- Favicon -->
+    <link href="<?= base_url('assets/eshop/'); ?>img/favicon.ico" rel="icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Source+Code+Pro:700,900&display=swap" rel="stylesheet">
+
+    <!-- CSS Libraries -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/eshop/'); ?>lib/slick/slick.css" rel="stylesheet">
+    <link href="<?= base_url('assets/eshop/'); ?>lib/slick/slick-theme.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="<?= base_url('assets/eshop/'); ?>css/style.css" rel="stylesheet">
 </head>
-<!--/head-->
 
 <body>
-    <header id="header">
-        <section id="form">
-            <!--form-->
-            <div class="container">
-                <center>
-                    <img src="<?= base_url('assets/img/logo.png'); ?>" width="200px" alt="logo">
-                </center>
-                <br>
-                <div class="row">
-                    <div class="col-sm-4 col-sm-offset-1">
-                        <div class="login-form">
-                            <!--login form-->
-                            <h2>Login to your account</h2>
-                            <form action="<?= base_url('home/log'); ?>" method="post">
-                                <input type="email" placeholder="Email Address" name="email" id="email" />
-                                <input type="password" placeholder="Password" name="password" id="password" />
-                                <button type="submit" class="btn btn-default">Login</button>
-                            </form>
-                        </div>
-                        <!--/login form-->
-                    </div>
-                    <div class="col-sm-1">
-                        <h2 class="or">OR</h2>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="signup-form">
-                            <!--sign up form-->
-                            <h2>New User Signup!</h2>
-                            <?= $this->session->flashdata('message'); ?>
-                            <form action="<?= base_url('home/regis'); ?>" method="POST">
-                                <input type="text" placeholder="Name" name="name" id="name" />
-                                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
-                                <input type="email" placeholder="Email Address" name="email" id="email" />
-                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
-                                <input type="password" placeholder="Password" name="password" id="password" />
-                                <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
-                                <button type="submit" class="btn btn-default">Signup</button>
-                            </form>
-                        </div>
-                        <!--/sign up form-->
+    <!-- Nav Bar End -->
+
+    <!-- Bottom Bar Start -->
+    <div class="bottom-bar">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-md-3">
+                    <div class="logo">
+                        <a href="index.html">
+                            <img src="<?= base_url('assets/img/logo.png'); ?>" alt="logo">
+                        </a>
                     </div>
                 </div>
             </div>
-        </section>
-        <!--/form-->
+        </div>
+    </div>
+
+    <!-- Login Start -->
+    <div class="login">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="register-form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <b>New User Signup!</b>
+                                <?= $this->session->flashdata('message'); ?>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <form action="<?= base_url('home/regis'); ?>" method="POST">
+                                    <label>Name</label>
+                                    <input class="form-control" type="text" placeholder="Name">
+                                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                            <div class="col-md-6">
+                                <label>E-mail</label>
+                                <input class="form-control" type="email" placeholder="E-mail" name="email" id="email">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                            <div class="col-md-6">
+                                <label>Password</label>
+                                <input class="form-control" type="password" placeholder="Password">
+                                <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                            <div class="col-md-12">
+                                <button type="submit " class="btn">Submit</button>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="login-form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <b>Login to your account!</b>
+                            </div>
+                            <?= $this->session->flashdata('message'); ?>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <form action="<?= base_url('home/log'); ?>" method="post">
+                                    <label>E-mail</label>
+                                    <input class="form-control" type="email" placeholder="E-mail" name="email" id="email">
+
+                            </div>
+                            <div class="col-md-6">
+                                <label>Password</label>
+                                <input class="form-control" type="password" placeholder="Password" name="password" id="password">
+
+                            </div>
+                            <div class="col-md-12">
+                                <button type="submit" class="btn">Login</button>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Login End -->
+
+    <!-- Footer Start -->
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url('assets/eshop/'); ?>lib/easing/easing.min.js"></script>
+    <script src="<?= base_url('assets/eshop/'); ?>lib/slick/slick.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="<?= base_url('assets/eshop/'); ?>js/main.js"></script>
+</body>
+
+</html>
