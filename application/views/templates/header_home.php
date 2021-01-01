f2a73e3ab5109a05d4bac9ff4fe547ed07878e79
iqbalfsr-coder <muh18557@gmail.com>
2021-01-01 13:22:24 +0700

    Ngayom

--- application/views/templates/header_home.php
+++ application/views/templates/header_home.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>E Store - eCommerce HTML Template</title>
+    <title><?= $title; ?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <!-- Favicon -->
